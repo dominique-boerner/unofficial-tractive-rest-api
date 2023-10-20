41fca0fb0fecf2eaedd8371f1980296718c45814
Dominique Börner <dominique@mukosoft.de>
2023-10-20 19:32:18 +0200

    feat: improve validation if /location is called
    * trackerId must be exactly 8 characters long
    * trackerId should not be empty
    * trackerId needs to be a string

--- src/modules/location/tracker.dto.ts
+++ src/modules/location/tracker.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class TrackerDto {
   @ApiProperty()
   @IsNotEmpty()
+  @Length(8, 8, {
+    message: 'trackerId must be 8 characters long',
+  })
+  @IsString()
   trackerId: string;
 }
